a5a178b2eedf169359a0dc9824828e9422fd5343
Abba Sali <abbasaliaboubakar@gmail.com>
2023-09-27 15:20:28 +0100

    add services page

--- frontend/components/Profiles/index.tsx
+++ frontend/components/Profiles/index.tsx
@@ -1,9 +1,40 @@
 import {Avatar, Box, Button, Card, Dialog, Flex, Text, TextField} from "@radix-ui/themes";
 import {FaMailBulk, FaMapMarker, FaPhone} from "react-icons/fa";
+import Link from "next/link";
+import {useEffect, useState} from "react";
 
 const Profiles = () => {
 
+    const [path, setPath] = useState("");
+    useEffect(() => {
+        setPath(window.location.pathname);
+    }, []);
+
     const annonces = [
+        {
+            name: "Abba Sali",
+            position: "Software Developer",
+            phone: "691 84 69 22",
+            mail: "abbasaliaboubakar@gmail.com",
+            marker: "Maroua",
+            sex: "M"
+        },
+        {
+            name: "Abba Sali",
+            position: "Software Developer",
+            phone: "691 84 69 22",
+            mail: "abbasaliaboubakar@gmail.com",
+            marker: "Maroua",
+            sex: "F"
+        },
+        {
+            name: "Abba Sali",
+            position: "Software Developer",
+            phone: "691 84 69 22",
+            mail: "abbasaliaboubakar@gmail.com",
+            marker: "Maroua",
+            sex: "F"
+        },
         {
             name: "Abba Sali",
             position: "Software Developer",
@@ -44,68 +75,126 @@ const Profiles = () => {
                     <h4 className={'text-center text-3xl font-semibold'}>Ils <span
                         className={'bg-red-500 text-white px-2 py-1'}>répondent</span> à vos demandes
                     </h4>
-                    <button
+                    <Link
+                        href={"/services"}
                         className={"rounded-full font-semibold underline underline-offset-4 text-red-500 text-base"}>Voir
                         tous les services
-                    </button>
+                    </Link>
                 </div>
                 <div className={'my-10 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'}>
                     {
                         annonces.map((item, index) => {
                             return (
-                                <Card key={index}>
-                                    <Flex gap="3" align="center">
-                                        <Avatar
-                                            size="3"
-                                            radius="full"
-                                            fallback={item.sex === "M" ? "M" : "F"}
-                                        />
-                                        <Box>
-                                            <Text as="div" size="2" weight="bold">
-                                                {item.name}
-                                            </Text>
-                                            <Text as="div" size="2" color="gray">
-                                                {item.position}
-                                            </Text>
+
+                                path !== "/services" ?
+                                    index < 4 && (<Card key={index}>
+                                            <Flex gap="3" align="center">
+                                                <Avatar
+                                                    size="3"
+                                                    radius="full"
+                                                    fallback={item.sex === "M" ? "M" : "F"}
+                                                />
+                                                <Box>
+                                                    <Text as="div" size="2" weight="bold">
+                                                        {item.name}
+                                                    </Text>
+                                                    <Text as="div" size="2" color="gray">
+                                                        {item.position}
+                                                    </Text>
+                                                </Box>
+                                            </Flex>
+                                            <Box p={"3"}>
+                                                <hr/>
+                                                <Flex pt={"4"} justify="between" align="center">
+                                                    <Flex>
+                                                        <FaPhone color="gray" size={14}/>
+                                                        <Text as="div" size="2" ml={"2"} color="gray">
+                                                            Telephone
+                                                        </Text>
+                                                    </Flex>
+                                                    <Text as="div" size="2" color="gray">
+                                                        {item.phone}
+                                                    </Text>
+                                                </Flex>
+                                                <Flex pt={"4"} justify="between" align="center">
+                                                    <Flex>
+                                                        <FaMailBulk color="gray" size={14}/>
+                                                        <Text as="div" size="2" ml={"2"} color="gray">
+                                                            Mail
+                                                        </Text>
+                                                    </Flex>
+                                                    <Text as="div" size="2" color="gray">
+                                                        {item.mail}
+                                                    </Text>
+                                                </Flex>
+                                                <Flex pt={"4"} justify="between" align="center">
+                                                    <Flex>
+                                                        <FaMapMarker color="gray" size={14}/>
+                                                        <Text as="div" size="2" ml={"2"} color="gray">
+                                                            Position
+                                                        </Text>
+                                                    </Flex>
+                                                    <Text as="div" size="2" color="gray">
+                                                        {item.marker}
+                                                    </Text>
+                                                </Flex>
+                                            </Box>
+                                        </Card>
+                                    ) : (
+                                    <Card key={index}>
+                                        <Flex gap="3" align="center">
+                                            <Avatar
+                                                size="3"
+                                                radius="full"
+                                                fallback={item.sex === "M" ? "M" : "F"}
+                                            />
+                                            <Box>
+                                                <Text as="div" size="2" weight="bold">
+                                                    {item.name}
+                                                </Text>
+                                                <Text as="div" size="2" color="gray">
+                                                    {item.position}
+                                                </Text>
+                                            </Box>
+                                        </Flex>
+                                        <Box p={"3"}>
+                                            <hr/>
+                                            <Flex pt={"4"} justify="between" align="center">
+                                                <Flex>
+                                                    <FaPhone color="gray" size={14}/>
+                                                    <Text as="div" size="2" ml={"2"} color="gray">
+                                                        Telephone
+                                                    </Text>
+                                                </Flex>
+                                                <Text as="div" size="2" color="gray">
+                                                    {item.phone}
+                                                </Text>
+                                            </Flex>
+                                            <Flex pt={"4"} justify="between" align="center">
+                                                <Flex>
+                                                    <FaMailBulk color="gray" size={14}/>
+                                                    <Text as="div" size="2" ml={"2"} color="gray">
+                                                        Mail
+                                                    </Text>
+                                                </Flex>
+                                                <Text as="div" size="2" color="gray">
+                                                    {item.mail}
+                                                </Text>
+                                            </Flex>
+                                            <Flex pt={"4"} justify="between" align="center">
+                                                <Flex>
+                                                    <FaMapMarker color="gray" size={14}/>
+                                                    <Text as="div" size="2" ml={"2"} color="gray">
+                                                        Position
+                                                    </Text>
+                                                </Flex>
+                                                <Text as="div" size="2" color="gray">
+                                                    {item.marker}
+                                                </Text>
+                                            </Flex>
                                         </Box>
-                                    </Flex>
-                                    <Box p={"3"}>
-                                        <hr/>
-                                        <Flex pt={"4"} justify="between" align="center">
-                                            <Flex>
-                                                <FaPhone color="gray" size={14}/>
-                                                <Text as="div" size="2" ml={"2"} color="gray">
-                                                    Telephone
-                                                </Text>
-                                            </Flex>
-                                            <Text as="div" size="2" color="gray">
-                                                {item.phone}
-                                            </Text>
-                                        </Flex>
-                                        <Flex pt={"4"} justify="between" align="center">
-                                            <Flex>
-                                                <FaMailBulk color="gray" size={14}/>
-                                                <Text as="div" size="2" ml={"2"} color="gray">
-                                                    Mail
-                                                </Text>
-                                            </Flex>
-                                            <Text as="div" size="2" color="gray">
-                                                {item.mail}
-                                            </Text>
-                                        </Flex>
-                                        <Flex pt={"4"} justify="between" align="center">
-                                            <Flex>
-                                                <FaMapMarker color="gray" size={14}/>
-                                                <Text as="div" size="2" ml={"2"} color="gray">
-                                                    Position
-                                                </Text>
-                                            </Flex>
-                                            <Text as="div" size="2" color="gray">
-                                                {item.marker}
-                                            </Text>
-                                        </Flex>
-                                    </Box>
-                                </Card>
+                                    </Card>
+                                )
                             )
                         })
                     }
---
+++ frontend/pages/services/index.tsx
@@ -0,0 +1,14 @@
+import AppLayout from "../layout/app";
+import Profiles from "../../components/Profiles";
+
+const Services = () => {
+    return (
+        <AppLayout>
+            <main style={{background: '#eeffeb'}}>
+                <Profiles/>
+            </main>
+        </AppLayout>
+    );
+}
+
+export default Services;
